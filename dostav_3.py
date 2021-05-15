a7f7950f5dc3a973403b36c9ee486e9aa7d1e41d
Ivan <radniforg@yandex.ru>
2021-05-15 19:48:42 +0300

    Unfinished test 3

---
+++ dostav_3.py
@@ -0,0 +1,88 @@
+import psycopg2
+import datetime
+
+# для каждого города по дням создания заказа вывести следующие метрики. для заказов созданных в январе 2021:
+# - кол-во созданных заказов
+# - % отмененных заказов
+# - кол-во активных клиентов (активный = тот кто создавал заказы)
+# - кол-во заказов выполненных в тот же день
+# - медианное время выполнения заказа (предполагаем, что запрос выполняется на последней версии postgresql/mysql/mariadb)
+
+
+def database_search(dbname='dostavista', user='postgres', password='Grof240192#', host='localhost'):
+    conn = psycopg2.connect(dbname=dbname, user=user,
+                            password=password, host=host)
+    with conn:
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM orders WHERE created_dt < '2021-01-01' ")
+        before = []
+        for row in cur:
+            before.append(row)
+        cur.execute("SELECT * FROM orders WHERE created_dt > '2020-12-31' "
+                    "AND created_dt < '2021-02-01' ")
+        january = []
+        for row in cur:
+            january.append(row)
+        city_list = []
+        for row in before:
+            if row[1] not in city_list:
+                city_list.append(row[1])
+        # А теперь пропишем логику происходящего
+        # Чтобы не ротировать БД много раз, нужно вытащить все данные за один раз.
+        # По хорошему мне нужно сразу сделать словарь с ключом в виде ID города.
+        # Внутри словаря под ключом будет массив кортежей (?), где каждый кортеж - нужная информация по заказу.
+        # Либо можно сразу собирать статистику через плюсы. Тогда это будет словарь в словаре?
+        # метрики: созданные заказы; % отмененных заказов; активные клиенты;
+        # заказы день в день; медианное время выполнения заказа
+        # dict = {city_id: {orders: a, cancelled: b, active: [1, 2, ... , n], daily: c, total_time: d}}
+        report = {}
+        for city in city_list:
+            report[city] = {'orders': 0, 'cancelled': 0, 'active': [], 'daily': 0, 'total_time': 0}
+        limit = datetime.timedelta(days=1)
+        for row in january:
+            if row[1] not in report.keys():
+                report[row[1]] = {'orders': 1, 'cancelled': 0,
+                                  'active': [row[2]], 'daily': 0,
+                                  'total_time': datetime.timedelta(days=0)}
+                if row[5] == 'cancelled':
+                    report[row[1]]['cancelled'] = 1
+                elif row[5] == 'completed':
+                    report[row[1]]['total_time'] = row[8] - row[4]
+                    if row[8] - row[4] < limit:
+                        report[row[1]]['daily'] = 1
+            else:
+                report[row[1]]['orders'] += 1
+                if row[5] == 'completed':
+                    report[row[1]]['total_time'] = report[row[1]]['total_time'] + row[8] - row[4]
+                    if row[8] - row[4] < limit:
+                        report[row[1]]['daily'] += 1
+                elif row[5] == 'cancelled':
+                    report[row[1]]['cancelled'] += 1
+                if row[2] not in report[row[1]]['active']:
+                    report[row[1]]['active'].append(row[2])
+        playtime = row[8] - row[4]
+        print(playtime)
+        # затем нужно еще раз пробежаться по ключам и выдать все в качестве файла и текста.
+        for key in report.keys():
+            if report[key]['orders'] != 0:
+                # нужно отсортировать ключи по возрастанию и придумать счет медианы (не среднего)
+                print(f'Город {key} за январь 2021:\n'
+                      f'Количество созданных заказов: {report[key]["orders"]}\n'
+                      f'Процент отмененных заказов: '
+                      f'{report[key]["cancelled"]/report[key]["orders"]*100}%\n'
+                      f'Количество активных клиентов: {len(report[key]["active"])}\n'
+                      f'Количество выполненных заказов в тот же день: '
+                      f'{report[key]["daily"]}\n'
+                      f'Медианное время выполнения заказа:')
+            else:
+                print(f'Город {key} за январь 2021:\n'
+                      f'Количество созданных заказов: 0\n'
+                      f'Процент отмененных заказов: 0%\n'
+                      f'Количество активных клиентов: 0\n'
+                      f'Количество выполненных заказов в тот же день: 0\n'
+                      f'Медианное время выполнения заказа: 0')
+        return report
+
+
+print(database_search())
--- playground.py
+++ playground.py
@@ -1,15 +1,38 @@
-a = (1, 'a')
-b = (2, 'b')
-c = (1, 'c')
-array = [a, b, c]
-dictionary = {}
-for letter in array:
-    dictionary_keys = dictionary.keys()
-    print(dictionary_keys)
-    if letter[0] not in dictionary_keys:
-        dict_update = {letter[0]: letter[1]}
-        dictionary.update(dict_update)
-    else:
-        addon = dictionary[letter[0]]+letter[1]
-        dictionary[letter[0]] = addon
-print(dictionary)
+import datetime
+
+def dictionary_test():
+    a = (1, 'a')
+    b = (2, 'b')
+    c = (1, 'c')
+    array = [a, b, c]
+    dictionary = {}
+    for letter in array:
+        dictionary_keys = dictionary.keys()
+        print(dictionary_keys)
+        if letter[0] not in dictionary_keys:
+            dict_update = {letter[0]: letter[1]}
+            dictionary.update(dict_update)
+        else:
+            addon = dictionary[letter[0]]+letter[1]
+            dictionary[letter[0]] = addon
+
+
+def datetime_test():
+    a = datetime.datetime.now()
+    b = datetime.datetime.combine(datetime.date(2021, 4, 12), datetime.time(12, 30, 42))
+    c = datetime.datetime.combine(datetime.date(2021, 4, 28), datetime.time(17, 24, 47))
+    d = a - b
+    e = a - c
+    f = c - b
+    g = (d+e)/2
+    return((d, e, g))
+
+
+def new_dict():
+    a = {}
+    a['b'] = {'c': 'd'}
+    return a
+
+
+d = [1, 2, 3, 0]
+print(len(d))
